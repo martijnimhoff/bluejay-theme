427ec523d3236e98489e5b4a8ad6cc225f3537cb
Martijn Imhoff <info@martijnimhoff.nl>
2017-03-05 19:27:48 +0100

    Add woocommerce template

---
+++ woocommerce.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: Full width page
+ *
+ * @package WordPress
+ * @subpackage Blue Jay
+ * @since Blue Jay 1.0.0
+ */
+?>
+
+
+<?php get_header(); ?>
+
+<main>
+	<div class="banner" style="background-image:url('<?php echo get_template_directory_uri() ?>/img/banner-small-01.jpg')">
+		<div class="banner-content">
+		</div>	
+	</div>
+
+	
+	<div class="container content">
+		<div class="col-md-12 col-sm-12 col-xs-12">
+			<?php woocommerce_content(); ?>
+		</div>
+	</div>
+
+<?php get_footer(); ?>
