de4dfe47dd0af6d4e88f6c5637ef18322f7cb6f4
Martijn Imhoff <info@martijnimhoff.nl>
2017-03-05 13:56:11 +0100

    Hide news section when there are no posts

--- front-page.php
+++ front-page.php
@@ -66,50 +66,62 @@
 			
 	</div>
 
-	<div class="news-section">
-		<div class="container text-center">
-			<h2>News</h2>
-			<hr>
+	<?php
+	$args = array('posts_per_page' => 3);
 
-			<?php
-			$args = array('posts_per_page' => 3);
+	$blogposts = get_posts($args);
 
-			$blogposts = get_posts($args);
+	if(!empty($blogposts)) :
 
-			foreach( $blogposts as $post ) : setup_postdata( $post ); ?>
+		?> 
+		<div class="news-section">
+			<div class="container text-center">
+				<h2>News</h2>
+				<hr>
 
-				<div class="col-md-4 col-sm-6 col-xs-12">
-					<a href="<?php echo get_permalink(); ?>" class="news-item">
-						<?php if ( has_post_thumbnail() ) : ?>
-							<div class="news-image">
-								<div class="hoveroverlay">
-									<div class="btn btn-blue btn-active">read more</div>
+
+				<?php
+
+				foreach( $blogposts as $post ) : setup_postdata( $post ); ?>
+
+					<div class="col-md-4 col-sm-6 col-xs-12">
+						<a href="<?php echo get_permalink(); ?>" class="news-item">
+							<?php if ( has_post_thumbnail() ) : ?>
+								<div class="news-image">
+									<div class="hoveroverlay">
+										<div class="btn btn-blue btn-active">read more</div>
+									</div>
+									<img src="<?php the_post_thumbnail_url( 'medium' ); ?>" alt="news item"/>
 								</div>
-								<img src="<?php the_post_thumbnail_url( 'medium' ); ?>" alt="news item"/>
-							</div>
-						<?php endif; ?>
-						<h3><?php the_title(); ?></h3>
-						<p class="date">
-							<i class="fa fa-calendar" aria-hidden="true"></i>
-							 <?php the_time('j F Y'); ?>
-						</p>
-						<p class="excerpt">
-							<?php the_excerpt(); ?>
-						</p>
-					</a>
-				</div>
+							<?php endif; ?>
+							<h3><?php the_title(); ?></h3>
+							<p class="date">
+								<i class="fa fa-calendar" aria-hidden="true"></i>
+								 <?php the_time('j F Y'); ?>
+							</p>
+							<p class="excerpt">
+								<?php the_excerpt(); ?>
+							</p>
+						</a>
+					</div>
 
-			<?php endforeach; ?>
-
-			<?php wp_reset_postdata(); ?>
+				<?php endforeach; ?>
 
 
-			<div class="clearfix"></div>
+				<div class="clearfix"></div>
 
-			<a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ) ?>" class="btn btn-blue btn-hover-black">More news</a>
+				<a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ) ?>" class="btn btn-blue btn-hover-black">More news</a>
 
+			</div>
 		</div>
-	</div>
+
+		<?php 
+		
+		wp_reset_postdata(); 
+
+	endif;
+
+	?>
 
 	<div class="blue-section">
 		<div class="container follow-us">
